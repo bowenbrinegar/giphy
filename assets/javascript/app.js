50620d805658a04bb1d3423d0b548b43dddf0f2a
Bowen <bowenbrinegar@Bowens-MacBook-Pro.local>
2017-09-29 21:57:39 -0400

    infinite scroll

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,28 @@
 $(document).ready(function()  {
 
-var gifs = ["Matrix", "Notebook", "Nobody", "King", "Barbie", "Psych", "College", "Trump", "Halloween", "Europe", "United States", "Coding",];
+var gifs = ["Matrix", "Notebook", "Nobody", "King", "Barbie", "Psych", "College", "Trump", "Halloween", "Europe", "United States", "Coding"];
 
 function displayGifs() {
 
-	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + $(this).attr('data-name') + "&api_key=z8RiFI1Cu7OaqAm0pWO8MEzbb5gbmNvC&limit=60";
-    
+	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + $(this).attr('data-name') + "&api_key=z8RiFI1Cu7OaqAm0pWO8MEzbb5gbmNvC&limit=20";
+    $.ajaxSetup({'cache': false});
     $.ajax({
       url: queryURL,    
-      method: 'GET',
+      method: 'GET'
     }).done(function(response) {
+        console.log(response);
 
-            $('#grid').masonry( 'remove', $('#grid').find('.grid-item') ).masonry('layout');
-            
-            for (i = 0; i < 60; i++) {
+           function clearGifs() {
+                $('#grid').masonry( 'remove', $('#grid').find('.grid-item') ).masonry('layout');
+                $('#grid').masonry({
+                  itemSelector: '.grid-item',
+                  columnWidth: 5,
+                });
+            }
+
+            clearGifs();
+
+            for (i = 0; i < 20; i++) {
               
                 var gif = $("<img class='box3'>").attr("src", response.data[i].images.fixed_width.url).css("height", response.data[i].images.fixed_width.height * 1.5);
                 var link = $("<a target='blank'>").attr("href", response.data[i].bitly_gif_url).append(gif);
@@ -27,13 +36,35 @@ function displayGifs() {
                     $('#grid').append( listItem )
                     $('#grid').masonry( 'appended', listItem);
             }
-    
+
+            $(window).scroll(function(){
+
+                if ($(window).scrollTop() >= ($(document).height() - $(window).height())* 0.9) {
+
+                    for (var x = 0; x < 20; x++) {
+              
+                        var gif = $("<img class='box3'>").attr("src", response.data[x].images.fixed_width.url).css("height", response.data[x].images.fixed_width.height * 1.5);
+                        var link = $("<a target='blank'>").attr("href", response.data[x].bitly_gif_url).append(gif);
+                        var still = $("<img class='box3'>").attr("src", response.data[x].images.fixed_width_still.url).css("height", response.data[x].images.fixed_width.height * 1.5);
+                        var rating = $("<h1 class='rating'>").append(response.data[x].rating);
+                        var div2 = $("<div class='gifhover box3'>").append(rating).append(link).css("height", response.data[x].images.fixed_width.height * 1.5);
+                        var div1 = $("<div class='wrap box3'>").append(still).append(div2).css("height", response.data[x].images.fixed_width.height * 1.5);
+                          
+                        var listItem = $("<div>").append(div1).addClass("grid-item").css("height", response.data[x].images.fixed_width.height * 1.5).attr("data-aos", "slide-up");
+                             
+                            $('#grid').append( listItem )
+                            $('#grid').masonry( 'appended', listItem);
+                    }
+                
+                };
+            });
     });
 
-            $('#grid').masonry({
-              itemSelector: '.grid-item',
-              columnWidth: 5,
-            });
+    $('#grid').masonry({
+      itemSelector: '.grid-item',
+      columnWidth: 5,
+    });
+
 };
 
 
@@ -50,17 +81,19 @@ function renderButtons() {
 
 $("#add-gif").on("click", function(event) {
 	event.preventDefault();
-	var gif = $("#gif-input").val().trim();
+    var gif = $("#gif-input").val().trim();
 	gifs.push(gif);
 	renderButtons();
 });
 
 
-$(document).on("click", ".gif", displayGifs);
+$(document).on("click", ".gif", displayGifs)
+
+
 renderButtons();
 
 
 
 
-})
+});
 
